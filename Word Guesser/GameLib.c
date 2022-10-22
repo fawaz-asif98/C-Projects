399b069ff0e17423724ff99042a7fb48884ce4da
fawaz-asif98 <111908718+fawaz-asif98@users.noreply.github.com>
2022-10-22 17:35:18 -0500

    Update GameLib.c

---
+++ Word Guesser/GameLib.c
@@ -0,0 +1,110 @@
+/* GameLib */
+
+#include "GameLib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "PhraseBank.txt"
+
+void StartGame(char ChosenPhrase[])
+{
+	char DashPhrase[MAX_INPUT] = {};
+	int i = 0;
+	int Choice = 0;
+
+	printf("\n\nWelcome to %d STRIKES - YOU'RE OUT - the CSE version\n\n", STRIKES);
+    
+	printf("Please pick a phrase from the following menu\n\n");
+	
+    while (PhraseBank[i] != "")
+	{
+		CheckPhrase(PhraseBank[i]);
+		DashIt(PhraseBank[i], DashPhrase);
+		printf("%d.\t%s\n", i+1, DashPhrase);
+		i++;
+	}
+	
+	printf("\nEnter choice : ");
+	scanf("%d", &Choice);
+	getchar();
+	
+	while (Choice < 1 || Choice > i)
+	{
+		printf("You entered an invalid choice.\nPlease reenter ");
+		scanf("%d", &Choice);
+	}
+	
+	/* copy the phrase from PhraseBank based on Choice into ChosenPhrase */ 
+	strcpy(ChosenPhrase, PhraseBank[Choice-1]);
+}
+
+void CheckPhrase(char *Phrase)
+{
+	char *FindDash = NULL;
+	
+	/* call strchr() with Phrase and a dash and store the result in FindDash.*/
+	/* If that value is not NULL, then you found a dash */
+	FindDash = strchr(Phrase, '-');
+	
+	if (FindDash != NULL)
+	{
+		printf("\n\"%s\" contains a dash in position %d!!\n", Phrase, abs(Phrase-FindDash)+1);
+		printf("\nYou broke the rules.  We can't play.  BYE!!\n\n");
+		exit(0);
+	}
+}
+
+int GuessALetter(char Phrase[], char DashedPhrase[], char UpperPhrase[])
+{
+	char Guess;
+	char *FindGuess = NULL;
+	char UpperPhraseCopy[MAX_INPUT];
+	int FoundALetter = 0;
+	
+	strcpy(UpperPhraseCopy, UpperPhrase);
+	
+	printf("\n\n%s", DashedPhrase);	
+	printf("\n\nGuess a letter : ");
+	scanf(" %c", &Guess);
+	getchar();
+	Guess = toupper(Guess);
+
+	FindGuess = strchr(UpperPhraseCopy, Guess);
+	
+	while (FindGuess != NULL)
+	{
+		FoundALetter = 1;
+		DashedPhrase[FindGuess - UpperPhraseCopy] = Phrase[FindGuess - UpperPhraseCopy];
+		/* Dereference FindGuess and set it to a dash */
+		*FindGuess = '-';
+
+		FindGuess = strchr(UpperPhraseCopy, Guess);
+	}
+	
+	return FoundALetter;
+}
+
+void DashIt(char *Phrase, char DashPhrase[])
+{
+	char *ReplaceIt;
+	char Alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i = 0;
+
+	/* Put the uppercase version of Phrase into DashPhrase */
+    strcpy(DashPhrase, Phrase);
+	
+	for (i; i < strlen(Phrase); i++ )
+	{
+	    DashPhrase[i] = toupper(Phrase[i]);
+	}
+	
+    ReplaceIt = strpbrk(DashPhrase, Alphabet);
+    DashPhrase[i] = '\0';
+    
+	while (ReplaceIt != NULL)
+	{
+		*ReplaceIt = '-';
+        ReplaceIt = strpbrk(DashPhrase, Alphabet);
+	}
+}
